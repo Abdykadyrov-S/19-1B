452317608a9d20622c2888154ced125d9b0bd967
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-07-16 20:20:46 +0600

    lessons 1-5

---
+++ lesson_5.py
@@ -0,0 +1,26 @@
+"""
+Команды пишутся только один раз когда вы делаете первый пуш
+для соединения vscode с github
+
+
+git --version
+
+git config --global user.name "YOUR USERNAME ON GITHUB"
+
+git config --global user.email "YOUR EMAIL ON GITHUB"
+
+
+"""
+
+
+"""
+Команды для пуша на новый репозиторий (первый пуш в новом репохитории)
+
+
+git init
+
+git add .
+# git add lesson_1.py
+
+
+"""
